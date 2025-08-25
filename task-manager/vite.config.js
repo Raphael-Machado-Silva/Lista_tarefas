44792e1aa68124459167c1c55d2b9f58ab97fc91
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-08-25 09:30:11 -0300

    update

--- task-manager/vite.config.js
+++ task-manager/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Lista_tarefas/', // nome do repo no GitHub
 })
